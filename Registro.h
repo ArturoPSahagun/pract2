50918f4a970a33e410d60954958292ac86d17fa7
Arturo Sahagun <arturo.bernal3648@alumnos.udg.mx>
2020-11-11 20:32:15 -0600

    Agregado el metodo para generar las direcciones base.

--- Registro.h
+++ Registro.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 class registro{
@@ -11,6 +12,7 @@ public:
         provincia[20];
 
     void leerRegistro(ifstream&);
+    int obtenerDireccionBase();
 };
 
 void registro::leerRegistro(ifstream& file){
@@ -18,4 +20,23 @@ void registro::leerRegistro(ifstream& file){
     file.getline(nombre, 30, '|');
     file.getline(domicilio, 20, '|');
     file.getline(provincia, 20);
+}
+
+int registro::obtenerDireccionBase(){
+    int formaNumerica[8];
+    for(int i = 0; i < 8; i++)
+        formaNumerica[i] = placa[i];
+
+    int numerosConcatenados[4];
+    for(int i = 0; i < 4; i++)
+        numerosConcatenados[i] = stoi( to_string(formaNumerica[i*2]) + to_string(formaNumerica[(i*2) + 1]));
+
+    int s=numerosConcatenados[0];
+
+    for(int i = 1; i < 4; i++){
+        s+=numerosConcatenados[i];
+        s = s%20000;
+    }
+
+    return s%101;
 }
